41a5ccfc3ee300671d1835b5abe9dd22c3485487
Afaq Javed <afaqjavedofficial@gmail.com>
2024-08-22 11:31:13 +0500

    added base

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AfaqJaved/afaqlogger
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package afaq
+
+import "fmt"
+
+func Logger(message string) {
+	fmt.Println("Logger: ", message)
+}
+
+func Logger2(message string) {
+	fmt.Println("Logger2: ", message)
+}
